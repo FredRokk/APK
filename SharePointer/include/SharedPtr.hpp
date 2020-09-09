d96c4da66a40fd49fd5680b848df4411f532acd3
FredRokk <you@example.com>
2020-09-09 15:08:30 +0200

    Started on definitions

--- SharePointer/include/SharedPtr.hpp
+++ SharePointer/include/SharedPtr.hpp
@@ -1,12 +1,21 @@
+#ifndef SHARED_PTR
+#define SHARED_PTR
 template <typename T> class SharedPtr
 {
 public:
-  SharedPtr(T *t);
-  explicit SharedPtr(const SharedPtr &);
+  SharedPtr(T *t)
+  {
+    
+  }
+  explicit SharedPtr(const SharedPtr &)
+  {
+    
+  }
   ~SharedPtr();
   SharedPtr &operator=(const SharedPtr &);
   T &        operator*() const;
   T *        operator->() const;
-  bool       operator==(const T &) const;
+  explicit bool       operator==(const T &) const;
   size_t     count() const;
 };
+#endif
