df8e2702eb6c18c0077708caf52b7e4b22fc0d8a
FredRokk <you@example.com>
2020-09-28 11:51:46 +0200

    Updated code with << overload

--- Boost/src/main.cpp
+++ Boost/src/main.cpp
@@ -1,15 +1,23 @@
 #include <boost/variant.hpp>
-#include <string>
 #include <iostream>
+#include <string>
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const boost::variant<T> &var)
+{
+  os << boost::get<T>(var);
+  return os;
+}
 
 int main()
 {
-    boost::variant<int, std::string, double> v;
-    v = 255;
-    std::cout << boost::get<int>(v) << std::endl;
-    v = "Hello, World!";
-    std::cout << boost::get<std::string>(v) << std::endl;
-    v = 3.1415;
-    std::cout << boost::get<double>(v) << std::endl;
-    return 0;
+  boost::variant<int, std::string, double> v;
+  v = 255;
+  std::cout << v << std::endl;
+  v = "Hello, World!";
+  std::cout << v << std::endl;
+  v = 3.1415;
+  std::cout << v << std::endl;
+
+  return 0;
 }
